7cc4518ab1f5c3ce19c4c0428def969540ae8e6c
ShahFH <shahfaisal.swe@gmail.com>
2023-08-09 14:04:05 +0430

    working

--- src/components/Pupolar.jsx
+++ src/components/Pupolar.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Pupolar() {
+  useEffect(() =>{
+    getPopular();
+  },[])
+
+  const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.RECIPE_API_KEY}&number=9`);
+    const data = await api.json();
+    console.log(data);
+
+  }
+
+
+
   return (
     <div>Pupolar</div>
   )
