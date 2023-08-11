cab23382b6895047e01cecdc91c84cc261b50034
ShahFH <shahfaisal.swe@gmail.com>
2023-08-11 15:29:17 +0430

    issue fixed

---
+++ src/pages/Recipee.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { Styled } from 'styled-components'
+import { useParams } from 'react-router-dom'
+
+
+function Recipee() {
+  return (
+    <div>Recipee</div>
+  )
+}
+
+export default Recipee
