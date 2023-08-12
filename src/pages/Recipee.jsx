871d6f7435e1629a3ac36962775513a84881e635
ShahFH <shahfaisal.swe@gmail.com>
2023-08-12 22:43:15 +0430

    fix

--- src/pages/Recipee.jsx
+++ src/pages/Recipee.jsx
@@ -5,10 +5,10 @@ import { useParams } from 'react-router-dom'
 
 
 function Recipee() {
-  
+
   const fetchDetails = async () => {
     let params = useParams();
-    const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}`);
+    const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}information?apiKey=${process.env.REACT_APP_API_KEY}`);
   }
 
   return (
